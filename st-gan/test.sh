44b4b18f83cf094c86fee9c4ec7b34719d7eee8e
PrasannaNatarajan <prasanna2431997@gmail.com>
2020-11-29 19:24:41 -0800

    removed invalid path from test.sh

--- st-gan/test.sh
+++ st-gan/test.sh
@@ -1,1 +1,1 @@
-python eval_STGAN.py --loadGP=final_model/test_warp5_it50000 --warpN=5 --loadImage=./my_data/1.jpg --batchSize 18 --size 128x128
+python eval_STGAN.py --loadGP=final_model/test_warp5_it50000 --warpN=5 --loadImage=my_data/1.jpg --batchSize 18 --size 128x128
